4f1a0ebb55a0e503eb5753f5e44cfa4dc7e8d70e
Tatiana Korol <tatjana.deriugina@gmail.com>
2025-02-16 15:04:53 +0100

    fix: fix CR comments

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -12,17 +12,15 @@ let age;
 
 if (isNaN(age)) {
   console.log('Incorrect data type');
-  return;
 } else {
   age = +age;
-}
-
-if (age < minAge) {
-  console.log(`You don't have access cause your age is ${age}. It's less than ${minAge}`);
-} else if (age >= minAge && age < maxAge) {
-  console.log('Welcome!');
-} else if (age > maxAge) {
-  console.log('Keep calm and look Culture channel');
-} else {
-  console.log('Technical work');
+  if (age < minAge) {
+    console.log(`You don't have access cause your age is ${age}. It's less than ${minAge}`);
+  } else if (age >= minAge && age < maxAge) {
+    console.log('Welcome!');
+  } else if (age > maxAge) {
+    console.log('Keep calm and look Culture channel');
+  } else {
+    console.log('Technical work');
+  }
 }
